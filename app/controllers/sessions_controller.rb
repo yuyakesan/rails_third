bf5cc2a40b854e0d9af508c6e28a5e1718b1ec57
Ubuntu <ubuntu@ip-172-31-15-242.us-east-2.compute.internal>
2021-04-01 12:29:45 +0000

    rails tutotrial sec10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
-  
-  #Post LOGIN
- def create
+
+  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
@@ -14,16 +13,10 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
- end
- 
- def current_user
-   if session[:user_id]
-     User.find_by(id:session[:user_id])
-   end
- end
- 
- def destroy
-     log_out if logged_in?
-     redirect_to root_url
- end 
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
 end
