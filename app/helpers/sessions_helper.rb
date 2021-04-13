ff94cafd0750288e55fcc41385221c1471607b52
Ubuntu <ubuntu@ip-172-31-15-242.us-east-2.compute.internal>
2021-04-13 11:28:43 +0000

    Quetion git

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -55,7 +55,7 @@ module SessionsHelper
     @current_user = nil
   end
 
-  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  # 記憶したURL（もしくはデフォルト値）にリダイレクト
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
     session.delete(:forwarding_url)
